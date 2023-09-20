b12f334a27d4732ac58e3db7afbe38c507cd0770
Jônatas T. Andrade <87863493+jonat4s@users.noreply.github.com>
2023-09-20 13:10:13 -0300

    method to calculate permeability by nmr

--- examples/example_BRSA_1053.py
+++ examples/example_BRSA_1053.py
@@ -17,30 +17,30 @@ prj.well_names_paths
 
 # %%
 
-prj.import_well('3-BRSA-1064-RJS_NMR_merge')
+prj.import_well('3-BRSA-1053-RJS_NMR_SLB_merge')
 
 data = prj.well_data
 
-for mnm in data['3-BRSA-1064-RJS_NMR_merge']:
+for mnm in data['3-BRSA-1053-RJS_NMR_SLB_merge']:
     print(mnm)
 
 
 # %%
 
-print(data['3-BRSA-1064-RJS_NMR_merge']['TDEP']['data'])
-print(data['3-BRSA-1064-RJS_NMR_merge']['TDEP']['unit'])
+print(data['3-BRSA-1053-RJS_NMR_SLB_merge']['MD']['data'])
+print(data['3-BRSA-1053-RJS_NMR_SLB_merge']['MD']['unit'])
 
-PHIT = data['3-BRSA-1064-RJS_NMR_merge']['TCMR']['data']
-PHIE = data['3-BRSA-1064-RJS_NMR_merge']['CMRP_3MS']['data']
-T2LM = data['3-BRSA-1064-RJS_NMR_merge']['T2LM']['data']
+PHIT = data['3-BRSA-1053-RJS_NMR_SLB_merge']['TCMR']['data']
+PHIE = data['3-BRSA-1053-RJS_NMR_SLB_merge']['CMRP_3MS']['data']
+T2LM = data['3-BRSA-1053-RJS_NMR_SLB_merge']['T2LM']['data']
 # %%
 
-MD = data['3-BRSA-1064-RJS_NMR_merge']['TDEP']['data']
+MD = data['3-BRSA-1053-RJS_NMR_SLB_merge']['MD']['data']
 VSHALE = shale_volume.vshale('ehigie',phit = PHIT,phie = PHIE)
 print(VSHALE)
 
 # %%
-FF = data['3-BRSA-1064-RJS_NMR_merge']['CMFF']['data']
+FF = data['3-BRSA-1053-RJS_NMR_SLB_merge']['CMFF']['data']
 SW = water_saturation.water_saturation( method = 'crain', phi = PHIE, ff = FF )
 print(SW)
 
@@ -48,27 +48,12 @@ ln = len(PHIT)
 
 #a1 = 1.
 # %%
-KSDR = data['3-BRSA-1064-RJS_NMR_merge']['KSDR']['data']
-K = permeability.permeability(method = 'sdr', a1 = 1., phit = PHIT / 100 , m1 = 2.6, t2_lm = T2LM, n1 = 2.6)
+KSDR = data['3-BRSA-1053-RJS_NMR_SLB_merge']['KSDR']['data']
+K = permeability.permeability(method = 'sdr', a1 = 1., phit = PHIT / 100 , m1 = 2, t2_lm = T2LM, n1 = 2)
 print(list(K))
 
 # %%
-h = 300
-_evaluation = []
-_parameters = []
-for _m in np.linspace(2,4,h):
-    for _n in np.linspace(2,4,h):
-        _parameters.append([_m,_n])
-        _K = permeability.permeability(method = 'sdr', a1 = 1., phit = PHIT / 100 , m1 = _m, t2_lm = T2LM, n1 = _n)
-        dif = abs(_K - KSDR)
-        _evaluation.append(1./np.mean(dif))
 
-c_max = np.nanmax(_evaluation)
-idx = _evaluation.index(c_max)
-print(c_max,idx,_parameters[idx])
-
-# %%
-K = permeability.permeability(method = 'sdr', a1 = 1., phit = PHIT / 100 , m1 = _parameters[idx][0], t2_lm = T2LM, n1 =_parameters[idx][1])
 plt.plot(K, KSDR,'.')
 plt.show()
 
--- stoneforge/petrophysics/__init__.py
+++ stoneforge/petrophysics/__init__.py
@@ -1,3 +1,4 @@
 from . import porosity  # noqa: F401
 from . import shale_volume  # noqa: F401
 from . import water_saturation  # noqa: F401
+from . import permeability  # noqa: F401
